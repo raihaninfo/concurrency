f7c709fa79f483f622e7c62de153a61024332b30
raihaninfo <raihanmahmudi35@gmail.com>
2022-02-20 22:17:53 +0600

    interface

---
+++ interface/main.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+
+type person struct {
+	first string
+	last  string
+}
+
+type secretAgent struct {
+	person
+	ltk bool
+}
+
+func (s secretAgent) speak() {
+	fmt.Println("I am", s.first, s.last, " - the secretAgent speak")
+}
+
+func (p person) speak() {
+	fmt.Println("I am", p.first, p.last, " - the person speak")
+}
+
+func main() {
+	sa1 := secretAgent{
+		person: person{
+			"James",
+			"Bond",
+		},
+		ltk: true,
+	}
+
+	sa2 := person{
+		first: "jon",
+		last:  "Due",
+	}
+
+	fmt.Println(sa1)
+	sa1.speak()
+	sa2.speak()
+}
