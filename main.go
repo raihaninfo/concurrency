0246e361e864d0ae64b63f1613062c23c39c382d
raihaninfo <raihanmahmudi35@gmail.com>
2022-02-20 14:28:27 +0600

    sample go channel

--- main.go
+++ main.go
@@ -2,21 +2,31 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
 func main() {
-	c := make(chan string)
-	go count("sheep", c)
-	for msg := range c {
-		fmt.Println(msg)
-	}
+	c := make(chan string, 2)
+	c <- "Hello"
+	c <- "World"
+	msg := <-c
+	fmt.Println(msg)
+
+	msg = <-c
+	fmt.Println(msg)
 }
 
-func count(thing string, c chan string) {
-	for i := 0; i <= 5; i++ {
-		c <- thing
-		time.Sleep(time.Millisecond * 500)
-	}
-	close(c)
-}
+// func main() {
+// 	c := make(chan string)
+// 	go count("sheep", c)
+// 	for msg := range c {
+// 		fmt.Println(msg)
+// 	}
+// }
+
+// func count(thing string, c chan string) {
+// 	for i := 0; i <= 5; i++ {
+// 		c <- thing
+// 		time.Sleep(time.Millisecond * 500)
+// 	}
+// 	close(c)
+// }
