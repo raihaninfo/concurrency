60aa6f5f3d99b2790c7f040ec3d0c3e5f3a203e3
raihaninfo <raihanmahmudi35@gmail.com>
2022-02-20 15:16:16 +0600

    another way to use channel

--- channel/main.go
+++ channel/main.go
@@ -2,16 +2,28 @@ package main
 
 import "fmt"
 
+// another syntax
 func main() {
-	dataChan := make(chan int, 2)
+	dataChan := make(chan int)
 
-	dataChan <- 789
-	dataChan <- 343
+	go func() {
+		for i := 0; i < 100; i++ {
+			dataChan <- i
+		}
+		close(dataChan)
+	}()
 
-	n := <-dataChan
-	fmt.Println(n)
-
-	n = <-dataChan
-	fmt.Println(n)
-
+	for n := range dataChan {
+		fmt.Println(n)
+	}
 }
+
+// func main() {
+// 	dataChan := make(chan int, 2)
+// 	dataChan <- 789
+// 	dataChan <- 343
+// 	n := <-dataChan
+// 	fmt.Println(n)
+// 	n = <-dataChan
+// 	fmt.Println(n)
+// }
