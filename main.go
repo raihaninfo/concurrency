6e6cb202f79834ddd9e7ee01bf89a72078d87188
raihaninfo <raihanmahmudi35@gmail.com>
2022-02-20 14:38:03 +0600

    go channel with goroutines

--- main.go
+++ main.go
@@ -2,18 +2,50 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 func main() {
-	c := make(chan string, 2)
-	c <- "Hello"
-	c <- "World"
-	msg := <-c
-	fmt.Println(msg)
+	c1 := make(chan string)
+	c2 := make(chan string)
 
-	msg = <-c
-	fmt.Println(msg)
+	go func() {
+		for {
+			c1 <- "Every 500ms"
+			time.Sleep(time.Millisecond * 500)
+		}
+	}()
+
+	go func() {
+		for {
+			c2 <- "Every two seconds"
+			time.Sleep(time.Second * 2)
+		}
+	}()
+
+	for {
+		// fmt.Println(<-c1)
+		// fmt.Println(<-c2)
+		select {
+		case msg1 := <-c1:
+			fmt.Println(msg1)
+		case msg2 := <-c2:
+			fmt.Println(msg2)
+		}
+	}
+
 }
+
+// func main() {
+// 	c := make(chan string, 2)
+// 	c <- "Hello"
+// 	c <- "World"
+// 	msg := <-c
+// 	fmt.Println(msg)
+
+// 	msg = <-c
+// 	fmt.Println(msg)
+// }
 
 // func main() {
 // 	c := make(chan string)
