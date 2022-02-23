ef58ff73724685dd60a600033bbc68cbc2357ecc
raihaninfo <raihanmahmudi35@gmail.com>
2022-02-23 10:59:00 +0600

    go channel

--- multifunc/main.go
+++ multifunc/main.go
@@ -3,33 +3,33 @@ package main
 import (
 	"fmt"
 	"sync"
-	"time"
 )
 
 var wg sync.WaitGroup
 
-func generateNumbers(total int) {
+func generateNumbers(total int, ch chan<- int) {
 	defer wg.Done()
 	for idx := 1; idx <= total; idx++ {
-		fmt.Printf("Generating number %d\n", idx)
-		time.Sleep(time.Second)
+		fmt.Printf("sending %d to channel\n", idx)
+		ch <- idx
 	}
 }
 
-func printNumbers() {
+func printNumbers(ch <-chan int) {
 	defer wg.Done()
-
-	for idx := 1; idx <= 10; idx++ {
-		fmt.Printf("Printing number %d\n", idx)
-		time.Sleep(time.Second)
+	for num := range ch {
+		fmt.Printf("read %d from channel\n", num)
 	}
 }
 
 func main() {
-
+	numberChan := make(chan int)
 	wg.Add(2)
-	go printNumbers()
-	go generateNumbers(10)
+	go printNumbers(numberChan)
+
+	generateNumbers(3, numberChan)
+
+	close(numberChan)
 
 	fmt.Println("Waiting for goroutines to finish...")
 	wg.Wait()
