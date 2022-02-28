7a72ae7c738a645ded598c73086023dde6fbf4c1
raihaninfo <raihanmahmudi35@gmail.com>
2022-02-28 21:28:21 +0600

    part 3  exercises 2 file

---
+++ resource_pull/part3/ex2/rp/pool.go
@@ -0,0 +1,28 @@
+package rp
+
+import "concurrency/resource_pull/part3/ex2/model"
+
+const CR_POOL_SIZE = 10000
+
+var pool chan *model.ClientReq
+
+func init() {
+	pool = make(chan *model.ClientReq, CR_POOL_SIZE)
+}
+
+func Alloc() *model.ClientReq {
+	select {
+	case cr := <-pool:
+		return cr
+	default:
+		cr := &model.ClientReq{}
+		return cr
+	}
+}
+
+func Release(cr *model.ClientReq) {
+	select {
+	case pool <- cr:
+	default:
+	}
+}
