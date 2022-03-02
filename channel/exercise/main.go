026ea2b16e55f7a6f831785d6c8ac59d3ff37298
raihaninfo <raihanmahmudi35@gmail.com>
2022-03-02 20:26:41 +0600

    fibonacci

--- channel/exercise/main.go
+++ channel/exercise/main.go
@@ -2,21 +2,38 @@ package main
 
 import "fmt"
 
-func sum(a []int, c chan int) {
-	sum := 0
-	for _, v := range a {
-		sum += v
+// func sum(a []int, c chan int) {
+// 	sum := 0
+// 	for _, v := range a {
+// 		sum += v
+// 	}
+// 	c <- sum
+// }
+
+// func main() {
+// 	a := []int{7, 2}
+
+// 	c := make(chan int)
+// 	go sum(a, c)
+// 	go sum(a, c)
+// 	x, y := <-c, <-c
+
+// 	fmt.Println(x, y)
+// }
+
+func fibonacci(n int, c chan int) {
+	x, y := 0, 1
+	for i := 0; i < n; i++ {
+		c <- x
+		x, y = y, x+y
 	}
-	c <- sum
+	close(c)
 }
 
 func main() {
-	a := []int{7, 2}
-
-	c := make(chan int)
-	go sum(a, c)
-	go sum(a, c)
-	x, y := <-c, <-c
-
-	fmt.Println(x, y)
+	c := make(chan int, 10)
+	go fibonacci(cap(c), c)
+	for i := range c {
+		fmt.Println(i)
+	}
 }
